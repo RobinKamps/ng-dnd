d8c05949301c83e5e1dc534e2acdc51344a71c8c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-11-05 14:09:03 +0000

    Deploy ng-dnd/ng-dnd to github.com/ng-dnd/ng-dnd.git:gh-pages

---
+++ examples/common-es5.13c8c8e0f3248b267546.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{NyMZ:function(A,n,a){"use strict";var e;function i(){return e||((e=new Image).src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="),e}a.d(n,"a",(function(){return i}))}}]);
